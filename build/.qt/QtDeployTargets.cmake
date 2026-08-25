6fc68285875bc87a5d9bbe4a3a4ab8d5990a5574
chornay126@gmail.com <chornay126@gmail.com>
2026-08-25 18:20:55 +0700

    update

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_PomodoroClock_FILE D:/ITE/Year 1/Semester 2/CPL/Pomewodoro/build/PomodoroClock.exe)
-set(__QT_DEPLOY_TARGET_PomodoroClock_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_PomodoroClock_RUNTIME_DLLS C:/msys64/ucrt64/bin/Qt6Widgets.dll;C:/msys64/ucrt64/bin/Qt6Gui.dll;C:/msys64/ucrt64/bin/Qt6Core.dll)
