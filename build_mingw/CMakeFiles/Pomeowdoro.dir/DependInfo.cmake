0ecdb23ca48e85fa7814cc295011a6d6cf5dea7f
Cristail-Rith <sovvanrith72@gmail.com>
2026-08-31 22:44:16 +0700

    release nrw

---
+++ build_mingw/CMakeFiles/Pomeowdoro.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "Pomeowdoro_autogen/timestamp" "custom" "Pomeowdoro_autogen/deps"
+  "C:/Users/Admin/OneDrive/Desktop/rean-vue/Pomewodoro/build_mingw/Pomeowdoro_autogen/mocs_compilation.cpp" "CMakeFiles/Pomeowdoro.dir/Pomeowdoro_autogen/mocs_compilation.cpp.obj" "gcc" "CMakeFiles/Pomeowdoro.dir/Pomeowdoro_autogen/mocs_compilation.cpp.obj.d"
+  "C:/Users/Admin/OneDrive/Desktop/rean-vue/Pomewodoro/main.cpp" "CMakeFiles/Pomeowdoro.dir/main.cpp.obj" "gcc" "CMakeFiles/Pomeowdoro.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
